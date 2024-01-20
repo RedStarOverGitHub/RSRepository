af1307f94df5d1acf2b438fe117ca3f8e7a286f3
RedStarCode <uhao01@outlook.com>
2024-01-20 11:43:00 +0800

    new file:   "Python/temp/\345\226\204\351\233\205\346\225\260.py"

---
+++ Python/temp/善雅数.py
@@ -0,0 +1,20 @@
+SYnDict = {}
+for i in range(3, 10):
+    for j in range(4, 10):
+        SYnDict[1000*i+100*(i-2)+10*(j-3)+j] = (10*(j-3)+j, 10*i+(i-2))
+
+
+def F(n):
+    s = SYnDict[n][0]
+    t = SYnDict[n][1]
+    if n in SYnDict:
+        return (((s+t)/3), s, t)
+    else: 
+        return None
+    
+
+for k in SYnDict:
+    if F(k)[0] != int(F(k)[0]):
+        continue
+    elif (10*F(k)[1]+F(k)[2]+1) % 13 == 0:
+        print(k)
