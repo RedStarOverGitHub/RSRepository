19dc516ede973a6b66bb1b187ac477f4295711b0
RedStarCode <uhao01@outlook.com>
2024-01-30 20:00:13 +0800

    modified:   "Python/\346\211\271\351\207\217\346\233\264\346\226\260Python\345\272\223.py"

--- Python/批量更新Python库.py
+++ Python/批量更新Python库.py
@@ -3,16 +3,18 @@ print('正在扫描需要更新的库...')
 # 定义需要更新的第三方库列表
 outdated = popen('pip list -o').read().split('\n')[2:-1]
 outdatedLibs = [(i.split()[0], i.split()[1], i.split()[2]) for i in outdated]
-if len(outdatedLibs) == 0:
+if len(outdatedLibs) == 0:  # 判断需要更新的库是否为空
     print('没有需要更新的库！')
 else:
+    # 列出需要更新的库
     print('以下库需要更新：')
     print('格式：库名: (当前版本, 最新版本)')
     for i in outdatedLibs:
         print(f'{i[0]}: ({i[1]}, {i[2]})')
     while True:
-        opt = input('是否更新？[Y/n]')
+        opt = input('是否更新？[Y/n]')  # 判断是否更新库
         if opt.lower() == 'y':
+            # 更新库
             for i in range(len(outdatedLibs)):
                 print(f'正在更新{outdatedLibs[i][0]}...（第{i+1}项，共{len(outdatedLibs)}项）')
                 system(f'pip install {outdatedLibs[i][0]} -U')
